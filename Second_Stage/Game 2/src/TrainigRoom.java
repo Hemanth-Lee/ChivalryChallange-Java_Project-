608240cd05f565f9719a437a598148e564df8d03
Lord Lee <hemanthofficial07@gmail.com>
2023-12-27 17:06:11 +0100

    for potion

--- Second_Stage/Game 2/src/TrainigRoom.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class TrainigRoom extends Area {
-    public TrainigRoom(String title, String description, int villainCount, ArrayList<Items> itemsInArea, ArrayList<Exits> exits, ArrayList<Coordinates> coordinates, ArrayList<Villain> villains){
-        super(title, description, villainCount, itemsInArea, exits, coordinates, villains);
-    }
-
-}
--- Second_Stage/Game 2/src_toHemanth/VillainBoss.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.List;
-public class VillainBoss extends Villain {
-	public VillainBoss (String name, String description, int xCoordinate, int yCoordinate, int healthPoints, int parryDenyCapacity, int damageCapacity, int blockCapacity) {
-		
-		super(name, description, xCoordinate, yCoordinate, healthPoints, parryDenyCapacity, damageCapacity, blockCapacity);
-
-}
-}
